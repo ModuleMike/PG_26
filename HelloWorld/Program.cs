e08088a28df659d0c87541bdff888d2ff408eb51
ModuleMike <146703098+ModuleMike@users.noreply.github.com>
2023-10-07 23:46:10 -0700

    Add files via upload

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,9 @@
+﻿string name;
+
+Console.WriteLine("Bread is Ready.");
+
+Console.WriteLine("Who is this bread for?");
+
+name = Console.ReadLine();
+
+Console.WriteLine($"Noted: {name} got bread.");
